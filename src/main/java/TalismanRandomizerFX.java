85df2dda1ebcb2b16524138085d6de41be699394
vinkstandard <merdacain@hotmail.it>
2025-05-28 15:23:59 +0200

    Modificato font e ridimensionata la finestra

--- src/main/java/TalismanRandomizerFX.java
+++ src/main/java/TalismanRandomizerFX.java
@@ -8,6 +8,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.AudioClip;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -21,40 +23,46 @@ public class TalismanRandomizerFX extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        System.out.println(getClass().getResource("/immagini/assassino.png"));
-
         VBox root = new VBox(15);
         root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-background-color: #000000;"); // colore azzurro chiaro
+
 
         Text title = new Text("Estrazione Personaggi");
 
-        Button generateButton = new Button("Estrai Personaggi");
-        VBox characterBox = new VBox(10);
-        characterBox.setAlignment(Pos.CENTER);
+        Button tastoInizia = new Button("Inizia");
+        VBox boxPersonaggi = new VBox(10);
+        boxPersonaggi.setAlignment(Pos.CENTER);
 
-        generateButton.setOnAction(e -> {
-            generateButton.setDisable(true); // Disabilita il bottone
+        tastoInizia.setOnAction(e -> {
+            tastoInizia.setDisable(true); // Disabilita il bottone
 
-            characterBox.getChildren().clear();
-            List<String> selected = getRandomCharacters(3);
+            boxPersonaggi.getChildren().clear();
+            List<String> selezionati = getRandomCharacters(3);
 
             // Creazione della timeline per mostrare 1 personaggio alla volta
             Timeline timeline = new Timeline();
-            for (int i = 0; i < selected.size(); i++) {
+            for (int i = 0; i < selezionati.size(); i++) {
                 final int index = i;
                 KeyFrame frame = new KeyFrame(Duration.seconds((index + 1) * 1.5), ev -> {
-                    String name = selected.get(index);
-                    Image img = new Image(getClass().getResource("/immagini/" + name.toLowerCase() + ".png").toExternalForm());
-                    ImageView view = new ImageView(img);
-                    view.setFitHeight(150);
-                    view.setFitWidth(100);
+                    String nome = selezionati.get(index);
+                    Image immagine = new Image(getClass().getResource("/immagini/" + nome.toLowerCase() + ".png").toExternalForm());
+                    ImageView view = new ImageView(immagine);
+                    view.setPreserveRatio(true);
+                    view.setFitHeight(300);
+                    view.setFitWidth(200);
 
                     VBox pgBox = new VBox(5);
                     pgBox.setAlignment(Pos.CENTER);
-                    pgBox.getChildren().addAll(view, new Text(name));
-                    characterBox.getChildren().add(pgBox);
 
-                    playSound(); // Riproduce il suono a ogni personaggio
+                    Text nomeTesto = new Text(nome.toUpperCase()); // nome elevato a uppercase per maggior leggibilità
+                    nomeTesto.setFill(javafx.scene.paint.Color.web("#ffd966")); // il colore del nome del personaggio
+                    nomeTesto.setFont(Font.font("System", FontWeight.BOLD, 20));
+                    pgBox.getChildren().addAll(view, nomeTesto);
+
+                    boxPersonaggi.getChildren().add(pgBox);
+
+                    playSound(); // riproduco il suono ogni volta che esce fuori un personaggio
                 });
                 timeline.getKeyFrames().add(frame);
             }
@@ -62,10 +70,10 @@ public class TalismanRandomizerFX extends Application {
             timeline.play();
         });
 
-        root.getChildren().addAll(title, generateButton, characterBox);
+        root.getChildren().addAll(title, tastoInizia, boxPersonaggi);
 
-        Scene scene = new Scene(root, 800, 600);
-        primaryStage.setScene(scene);
+        Scene scena = new Scene(root, 600, 800);
+        primaryStage.setScene(scena);
         primaryStage.setTitle("Talisman Randomizer FX");
         primaryStage.setResizable(false); // per bloccare il ridimensionamento
         primaryStage.show();
